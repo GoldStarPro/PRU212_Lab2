9d75ca0d9b82650de297b4520f68100d9c39aedc
DinhHellcat <DinhHellcat@gmail.com>
2025-03-19 20:59:38 +0700

    Dust and Boost speed| More particle system and Surface Effector

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,33 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float timeDelays = 2f;
     [SerializeField] ParticleSystem particleHit;
     Rigidbody2D rb;
+
+    SurfaceEffector2D se;
+    [SerializeField] float boostedSpeed = 30f;
+    [SerializeField] float normalSpeed = 15f;
+
+    [SerializeField] ParticleSystem Dust;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        se = FindObjectOfType<SurfaceEffector2D>();
     }
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        Rotate();
+        Boost();
+    }
+
+    void Boost()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            se.speed = boostedSpeed;
+        else
+            se.speed = normalSpeed;
+    }
+    void Rotate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
             rb.AddTorque(torque);
         else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
             rb.AddTorque(-torque);
@@ -26,6 +46,22 @@ public class PlayerController : MonoBehaviour
             particleHit.Play();
             Debug.Log("Ouch!");
             Invoke("LoadScene", timeDelays);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Dust.Play();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Dust.Stop();
         }
     }
 
